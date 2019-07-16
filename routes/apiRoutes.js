2923fc382689688dc28be0ef7b52c4b729d36d76
Andrew Joung <andrew.joung@gmail.com>
2019-07-16 11:54:01 -0700

    create mainpage with styling

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,24 @@
 var db = require("../models");
+var axios = require('axios');
 
 module.exports = function(app) {
   // Get all examples
   app.get("/api/examples", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.json(dbExamples);
+    });
+  });
+
+  app.get("/getweather",function(req, res) {
+    var apiKey = "3157d00bf198cb3d5c714f82d8eac54f";
+    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?zip=98006,us&appid=" + apiKey
+    console.log(queryUrl);
+    axios.get(queryUrl).then(function(data) {
+      console.log(data.data);
+      console.log(queryUrl);
+      res.json(data.data);
+    }).catch(function(error){
+      throw error;
     });
   });
 
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,16 +1,21 @@
 var db = require("../models");
+var axios = require('axios');
 
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    // db.Example.findAll({}).then(function(dbExamples) {
+    //   res.render("index", {
+    //     msg: "Welcome!",
+    //     examples: dbExamples
+    //   });
+    // });
+    res.send("This will be the register page");
   });
 
+  app.get("/main", function(req, res) {
+    res.render("index");
+  });
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
