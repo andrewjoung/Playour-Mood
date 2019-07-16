c2506a911609a542fe6bea78a6b45355ad220ece
Monali Chandurkar <chandurkar.monali123@gmail.com>
2019-07-16 13:42:59 -0700

    changed true false values

--- models/user_data.js
+++ models/user_data.js
@@ -2,12 +2,12 @@ module.exports = function(sequelize, DataTypes) {
   var user_data = sequelize.define("user_data", {
    uname:{
      type:DataTypes.STRING,
-     allowNull:False,
-     primaryKey:TRUE
+     allowNull:false,
+     primaryKey:true
    },
    password:{
      type:DataTypes.STRING,
-     allowNull:False,
+     allowNull:false,
      len:[8,16]
    },
    fav_genre:{
