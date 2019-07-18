bbcabccd955630d28a12445869a1b077c8537fc9
Andrew Joung <andrew.joung@gmail.com>
2019-07-18 11:01:27 -0700

    display songs in modal

--- public/js/index.js
+++ public/js/index.js
@@ -1,102 +1,3 @@
-// // Get references to page elements
-// var $exampleText = $("#example-text");
-// var $exampleDescription = $("#example-description");
-// var $submitBtn = $("#submit");
-// var $exampleList = $("#example-list");
-
-// // The API object contains methods for each kind of request we'll make
-// var API = {
-//   saveExample: function(example) {
-//     return $.ajax({
-//       headers: {
-//         "Content-Type": "application/json"
-//       },
-//       type: "POST",
-//       url: "api/examples",
-//       data: JSON.stringify(example)
-//     });
-//   },
-//   getExamples: function() {
-//     return $.ajax({
-//       url: "api/examples",
-//       type: "GET"
-//     });
-//   },
-//   deleteExample: function(id) {
-//     return $.ajax({
-//       url: "api/examples/" + id,
-//       type: "DELETE"
-//     });
-//   }
-// };
-
-// // refreshExamples gets new examples from the db and repopulates the list
-// var refreshExamples = function() {
-//   API.getExamples().then(function(data) {
-//     var $examples = data.map(function(example) {
-//       var $a = $("<a>")
-//         .text(example.text)
-//         .attr("href", "/example/" + example.id);
-
-//       var $li = $("<li>")
-//         .attr({
-//           class: "list-group-item",
-//           "data-id": example.id
-//         })
-//         .append($a);
-
-//       var $button = $("<button>")
-//         .addClass("btn btn-danger float-right delete")
-//         .text("ｘ");
-
-//       $li.append($button);
-
-//       return $li;
-//     });
-
-//     $exampleList.empty();
-//     $exampleList.append($examples);
-//   });
-// };
-
-// // handleFormSubmit is called whenever we submit a new example
-// // Save the new example to the db and refresh the list
-// var handleFormSubmit = function(event) {
-//   event.preventDefault();
-
-//   var example = {
-//     text: $exampleText.val().trim(),
-//     description: $exampleDescription.val().trim()
-//   };
-
-//   if (!(example.text && example.description)) {
-//     alert("You must enter an example text and description!");
-//     return;
-//   }
-
-//   API.saveExample(example).then(function() {
-//     refreshExamples();
-//   });
-
-//   $exampleText.val("");
-//   $exampleDescription.val("");
-// };
-
-// // handleDeleteBtnClick is called when an example's delete button is clicked
-// // Remove the example from the db and refresh the list
-// var handleDeleteBtnClick = function() {
-//   var idToDelete = $(this)
-//     .parent()
-//     .attr("data-id");
-
-//   API.deleteExample(idToDelete).then(function() {
-//     refreshExamples();
-//   });
-// };
-
-// // Add event listeners to the submit and delete buttons
-// $submitBtn.on("click", handleFormSubmit);
-// $exampleList.on("click", ".delete", handleDeleteBtnClick);
 
 $(document).ready(function () {
 
@@ -130,9 +31,13 @@ $(document).ready(function () {
 
   } else if (window.location.pathname === "/main") {
 
+    var dataToUse;
+
     $.ajax("/getweather", {type:"GET"}).then(function(data){
       console.log(data);
       console.log(data.weatherData.weather[0].main);
+
+      dataToUse = data;
       //console.log(data.spotifyData.playlists);
       //console.log(data.spotifyData);
       //console.log(data.spotifyData);
@@ -147,12 +52,15 @@ $(document).ready(function () {
       if(weather.toLowerCase() === "clouds" || weather.toLowerCase() === "haze" || weather.toLowerCase() === "fog") {
         console.log("entering cloud if");
         $('body').css("background-image", "url(../images/cloudy/cloudy" + randomNum + ".jpg)");
+        $('#modalHeader').addClass('cloudy-modal');
         $("#weatherIcon").addClass("fas fa-cloud fa-5x");
       } else if (weather.toLowerCase() === "clear") { //sunny weather
         $('body').css("background-image", "url(../images/sunny/sunny" + randomNum + ".jpg)");
+        $('#modalHeader').addClass('sunny-modal');
         $("#weatherIcon").addClass("fas fa-sun fa-5x");
       } else if (weather.toLowerCase() === "rain") {
         $('body').css("background-image", "url(../images/rainy/rainy" + randomNum + ".jpg)");
+        $('#modalHeader').addClass('rainy-modal');
         $("#weatherIcon").addClass("fas fa-cloud-showers-heavy fa-5x");
       }
 
@@ -164,7 +72,28 @@ $(document).ready(function () {
     //TODO:
     //Spotify API call
     $("#playlistButton").on("click", function(){
-      
+      console.log(dataToUse);
+      var modal = $("#modalBody");
+
+      for(var i = 0; i < dataToUse.songsToUse.length; i++) {
+        var songDiv = $("<div>");
+        songDiv.addClass("songDiv");
+        songDiv.addClass('row');
+
+        var songTitle = $("<p>");
+        songTitle.addClass('col-6');
+
+        var artist = $("<p>");
+        artist.addClass('col-6');
+
+        var line = $("<hr>");
+
+        songTitle.text(dataToUse.songsToUse[i].track.name);
+        artist.text(dataToUse.songsToUse[i].track.artists[0].name);
+
+        songDiv.append(songTitle).append(artist).append(line);
+        modal.append(songDiv);
+      }
     });
   }
 
